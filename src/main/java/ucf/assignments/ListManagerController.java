d55719127227c9e2276d3e8c591d03e1e50d0b19
Liam <xlime1p@gmail.com>
2021-06-29 14:35:03 -0400

    Initial solution (not finished)

---
+++ src/main/java/ucf/assignments/ListManagerController.java
@@ -0,0 +1,47 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class ListManagerController {
+    public void addListClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeListClicked(ActionEvent actionEvent) {
+    }
+
+    public void editListTitleClicked(ActionEvent actionEvent) {
+    }
+
+    public void addItemClicked(ActionEvent actionEvent) {
+    }
+
+    public void removeItemClicked(ActionEvent actionEvent) {
+    }
+
+    public void editDescClicked(ActionEvent actionEvent) {
+    }
+
+    public void editDueDateClicked(ActionEvent actionEvent) {
+    }
+
+    public void markAsCompleteClicked(ActionEvent actionEvent) {
+    }
+
+    public void displayIncompleteClicked(ActionEvent actionEvent) {
+    }
+
+    public void displayCompletedClicked(ActionEvent actionEvent) {
+    }
+
+    public void exportListClicked(ActionEvent actionEvent) {
+    }
+
+    public void exportAllListsClicked(ActionEvent actionEvent) {
+    }
+
+    public void importListClicked(ActionEvent actionEvent) {
+    }
+
+    public void importListsClicked(ActionEvent actionEvent) {
+    }
+}
